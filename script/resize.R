ceaed6b009a6b1a115024b4ed452f431385f1073
unknown <59862637+suzuna@users.noreply.github.com>
2021-08-01 03:46:47 +0900

    リサイズのパス周りを改善
    リサイズ前の画像が子フォルダ以下に分かれて存在する場合、リサイズ後の画像も子フォルダ以下を作成してその中に画像を入れる（フォルダ構成を維持する）

--- script/resize.R
+++ script/resize.R
@@ -4,6 +4,7 @@ library(magick)
 library(furrr)
 library(magrittr)
 library(here)
+library(fs)
 library(tictoc)
 
 
@@ -22,21 +23,37 @@ tile_colpx <- 50
 
 
 # 素材画像をタイルのpx数まで縮小する ------------------------------------------------------
-tile_path <- list.files(tile_img_path,pattern="(jpg|jpeg|png)$",full.names=T)
-tile_filename <- list.files(tile_img_path,pattern="(jpg|jpeg|png)$",full.names=F)
+tile_path_rel <- list.files(tile_img_path,pattern="(jpg|jpeg|png)$",full.names=F,recursive=T)
+tile_path_abs <- list.files(tile_img_path,pattern="(jpg|jpeg|png)$",full.names=T,recursive=T)
+
+# リサイズ前と後のパスの対照表
+df_resize_path <- data.frame(old_path_rel=tile_path_rel,old_path_abs=fs::path_abs(tile_path_abs)) %>% 
+  mutate(
+    new_path_dir_root=tile_resized_img_path,
+    new_path_abs=fs::path(new_path_dir_root,old_path_rel)
+  ) %>% 
+  # "_resized"を拡張子の前に挟む
+  mutate(new_path_abs=str_c(fs::path_ext_remove(new_path_abs),"_resized.",fs::path_ext(new_path_abs)))
+
+fs::path_dir(df_resize_path$new_path_abs) %>% 
+  unique() %>% 
+  walk(~{
+    if (!fs::dir_exists(.x)) {
+      # フォルダ"C:/Users/user"はあるが、フォルダ"C:/Users/user/a"もフォルダ"C:/Users/user/a/b"もない場合、
+      # fs::dir_create("C:/Users/user/a/b")とすればフォルダaを作りその下にフォルダbを作れる
+      # （base::dir.createは、2階層以上存在しないフォルダがある場合それはできない）
+      fs::dir_create(.x)
+    }
+  })
 
 plan(multisession)
 tictoc::tic()
-future_walk2(tile_path,tile_filename,~{
+future_walk2(df_resize_path$old_path_abs,df_resize_path$new_path_abs,~{
   tryCatch({
-    filename_before <- str_extract(.y,"[^/]*(?=\\.(jpeg|jpg|png)$)")
-    filename_ext <- str_extract(.y,"\\.(jpeg|jpg|png)$")
-    filename_after <- str_c(filename_before,"_resized",filename_ext)
     .x %>%
       magick::image_read() %>% 
       resize_img2(tile_colpx,tile_rowpx) %>% 
-      # TODO: 拡張子をファイルに合わせる
-      magick::image_write(file.path(tile_resized_img_path,filename_after))
+      magick::image_write(.y)
   },error=function(e){
     print(.x)
     print(e)
